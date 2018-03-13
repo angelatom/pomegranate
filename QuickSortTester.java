ea1d8deff7cb0b30ed17c2382e3cc0e071660ba2
angelatom <atom@stuy.edu>
2018-03-13 15:32:05 -0400

    Outline, no quicksort.java

---
+++ QuickSortTester.java
@@ -0,0 +1,25 @@
+/*
+Team pomegranate -- Angela Tom, Tabassum Fabiha, Shayan Chowdhury
+APCS2 pd2
+L01 -- What Does the Data Say?
+2018-03-14
+*/
+
+
+/* Summary of timing mechanism
+
+
+
+ */
+
+public class QuickSortTester{
+    static long startTime;
+    static long endTime;
+    public static void main(String[] args){
+	startTime = System.currentTimeMillis();
+	
+	endTime = System.currentTimeMillis();
+	System.out.println("Time elapsed: " + (endTime - startTime));
+      
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pomegranate
+Team pomegranate -- Angela Tom, Tabassum Fabiha, Shayan Chowdhury
+APCS2 pd2
+L01 -- What Does the Data Say?
+2018-03-14
+
+#Hypothesis
+
+
+#Background
+
+
+#Methodology
+
+
+#Results
+
+
+#Conclusions
+
+
